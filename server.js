4504a76c4370f2461ced83614b96d15dedfb6327
Kevin Tomas <kevin.tomas0123@gmail.com>
2018-07-07 09:45:14 -0700

    database change

--- server.js
+++ server.js
@@ -151,7 +151,7 @@ app.put("/delete/:id", function (req, res) {
     });
 });
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/anime";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
